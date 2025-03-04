04277d4beffdc97ce1b02ce29eb0a92ba10920a0
AlessandroJuleo <alessandrolc8003@gmail.com>
2025-03-03 20:27:49 -0600

    fix: cleanDb.ts

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -1,16 +1,23 @@
-import models from '../models/index.js';
-import db from '../config/connection.js';
+import models from "../models/index.js";
+import db from "../config/connection.js";
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
+    const model = models[modelName];
 
-    if (modelExists.length) {
+    if (!model || !model.db || !model.db.db) {
+      throw new Error(`Model ${modelName} is not properly initialized.`);
+    }
+
+    const collections = await model.db.db.listCollections({
+      name: collectionName,
+    }).toArray();
+
+    if (collections.length > 0) {
       await db.dropCollection(collectionName);
     }
   } catch (err) {
+    console.error("Error in cleanDb.ts:", err);
     throw err;
   }
-}
+};

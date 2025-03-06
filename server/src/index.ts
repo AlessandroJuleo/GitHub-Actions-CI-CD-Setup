16df584ad56d1427a877f277d270ce0dd2fda472
AlessandroJuleo <alessandrolc8003@gmail.com>
2025-03-06 14:56:15 -0600

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,10 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080; // Render usa 8080 por defecto
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API is running successfully!");
+});
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
